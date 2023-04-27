b3469645714b2224a0bfe9c208cc9b1b213f0e47
dudwlswmd <32324328+dudwlswmd@users.noreply.github.com>
2023-04-27 21:36:52 +0900

    Add files via upload

---
+++ js/foodJson.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+// const starFoodBtn = document.querySelector('.star_food_btn')
+// console.log(starFoodBtn)
+
+
+
+//      fetch('../data.json')
+//           .then(response => response.json())
+//           .then(dataaaa => {
+//                // 데이터 처리 로직
+//                let obj = Object.values(dataaaa.data);
+//                const ul = document.querySelector('.section5 ul');
+//                obj.forEach((el)=>{
+//                const li = document.createElement('li');
+//                li.innerHTML=
+//                `
+//                     <a href="">
+//                     <div class="img"><img src="${el.img}" alt=""><div class="icon"><span>추천</span><span>인기</span></div></div>
+//                     <div class="txt">
+//                          <span>${el.text}</span>
+//                          <h4>${el.name}</h4>
+//                     </div><!--txt : END-->
+//                     </a>
+//                `;
+          
+//                ul.append(li)
+
+
+               
+//           })
+//      }).catch();
+
+//      starFoodBtn.addEventListener('click',function(){
+// });
+
+
+// const starFoodBtn = document.querySelector('.star_food_btn');
+// console.log(starFoodBtn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
